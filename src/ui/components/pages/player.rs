9d1df5c3a37a681c861e90eec57de8a1bf71f405
Anant Narayan <anantnrg@gmail.com>
2026-02-05 19:09:28 +0530

    Thumbnail display function

--- src/ui/components/pages/player.rs
+++ src/ui/components/pages/player.rs
@@ -23,45 +23,31 @@ impl Render for PlayerPage {
             .flex()
             .items_center()
             .justify_center()
-            // .child(
-            //     div()
-            //         .id("load")
-            //         .w_40()
-            //         .h_8()
-            //         .flex()
-            //         .items_center()
-            //         .justify_center()
-            //         .bg(theme.bg_queue)
-            //         .rounded_md()
-            //         .child("Load")
-            //         .on_click(|_, _, cx| {
-            //             cx.global::<Controller>().load(
-            //                 "E:\\music\\violence ft. doomguy\\454 - Bad and Boujee.mp3".to_string(),
-            //             )
-            //         }),
-            // )
-            .child(
-                if let Some(meta) = cx.global::<Controller>().player_state.meta.clone() {
-                    if let Some(thumbnail) = meta.thumbnail {
-                        let format = match thumbnail.format.as_str() {
-                            "png" => ImageFormat::Png,
-                            "jpeg" | "jpg" => ImageFormat::Jpeg,
-                            _ => ImageFormat::Bmp,
-                        };
-                        div().bg(theme.white_10).size_56().child(
-                            img(ImageSource::Image(Arc::new(Image::from_bytes(
-                                format,
-                                thumbnail.image,
-                            ))))
-                            .object_fit(ObjectFit::Contain)
-                            .size_full(),
-                        )
-                    } else {
-                        div()
-                    }
-                } else {
-                    div()
-                },
+    }
+}
+
+fn thumbnail_display(cx: &mut App) -> impl IntoElement {
+    let theme = cx.global::<Theme>();
+
+    if let Some(meta) = cx.global::<Controller>().player_state.meta.clone() {
+        if let Some(thumbnail) = meta.thumbnail {
+            let format = match thumbnail.format.as_str() {
+                "png" => ImageFormat::Png,
+                "jpeg" | "jpg" => ImageFormat::Jpeg,
+                _ => ImageFormat::Bmp,
+            };
+            div().bg(theme.white_10).size_56().child(
+                img(ImageSource::Image(Arc::new(Image::from_bytes(
+                    format,
+                    thumbnail.image,
+                ))))
+                    .object_fit(ObjectFit::Contain)
+                    .size_full(),
             )
+        } else {
+            div()
+        }
+    } else {
+        div()
     }
 }
