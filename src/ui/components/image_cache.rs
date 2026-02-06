15beae43fc144ffb6bffeef0de16359fc0ec8ebc
Anant Narayan <anantnrg@gmail.com>
2026-02-06 19:05:13 +0530

    image cache

---
+++ src/ui/components/image_cache.rs
@@ -0,0 +1,43 @@
+use crate::controller::player::Track;
+use crate::ui::components::pages::player::get_img_format;
+use ahash::AHasher;
+use gpui::*;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+use std::path::PathBuf;
+use std::sync::Arc;
+
+#[derive(Default)]
+pub struct ImageCache {
+    map: HashMap<u64, Arc<Image>>,
+}
+
+fn image_key(path: &PathBuf) -> u64 {
+    let mut h = AHasher::default();
+    path.hash(&mut h);
+    h.finish()
+}
+
+pub fn get_or_create_image(
+    cx: &mut Context<impl ?Sized>,
+    track: &Track,
+) -> Option<Arc<Image>> {
+    let thumbnail = track.meta.thumbnail.as_ref()?;
+
+    let key = image_key(&track.path);
+    let cache = cx.global_mut::<ImageCache>();
+
+    if let Some(img) = cache.map.get(&key) {
+        return Some(img.clone());
+    }
+
+    let image = Arc::new(Image::from_bytes(
+        get_img_format(thumbnail.format.clone()),
+        thumbnail.image.clone(),
+    ));
+
+    cache.map.insert(key, image.clone());
+    Some(image)
+}
+
+impl Global for ImageCache {}
--- src/ui/components/mod.rs
+++ src/ui/components/mod.rs
@@ -4,6 +4,7 @@ pub mod pages;
 pub mod slider;
 pub mod titlebar;
 mod queue;
+mod image_cache;
 
 #[derive(Clone, Copy, PartialEq)]
 pub enum Page {
