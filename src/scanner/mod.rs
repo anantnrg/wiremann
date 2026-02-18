3f1575be3f242db9e41acdd9add6f6402c3eefec
Anant Narayan <anantnrg@gmail.com>
2026-02-18 19:46:56 +0530

    refactor(wip): scanning folder works.

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -34,12 +34,10 @@ impl Scanner {
         loop {
             match self.rx.recv()? {
                 ScannerCommand::GetTrackMetadata { path, track_id } => {
-                    println!("get track received!");
                     let track = self.get_track_metadata(path, track_id)?;
                     let _ = self.tx.send(ScannerEvent::Tracks(vec![track]));
                 }
                 ScannerCommand::ScanFolder { path, tracks } => {
-                    println!("scan folder received!");
                     self.scan_folder(path, tracks)?
                 }
             }
@@ -51,28 +49,83 @@ impl Scanner {
         path: PathBuf,
         track_id: TrackId,
     ) -> Result<Track, ScannerError> {
-        let tagged_file = Probe::open(path.clone())?.guess_file_type()?.read()?;
+        let tagged_file = match Probe::open(path.clone())
+            .and_then(|p| Ok(p.guess_file_type()?))
+            .and_then(|p| p.read())
+        {
+            Ok(file) => file,
+            Err(e) => {
+                eprintln!("Metadata decode failed {:?}: {:?}", path, e);
+
+                let file_metadata = fs::metadata(path.clone())?;
+                let duration = 0;
+                let size = file_metadata.len();
+                let modified = file_metadata
+                    .modified()?
+                    .duration_since(UNIX_EPOCH)?
+                    .as_secs();
+
+                let title = path
+                    .file_stem()
+                    .and_then(|s| s.to_str())
+                    .unwrap_or("Unknown")
+                    .to_string();
+
+                return Ok(Track {
+                    path,
+                    id: track_id,
+                    title,
+                    artist: "Unknown Artist".to_string(),
+                    album: "Unknown Album".to_string(),
+                    duration,
+                    modified,
+                    size,
+                });
+            }
+        };
+
         let file_metadata = fs::metadata(path.clone())?;
 
-        let tag = match tagged_file.primary_tag() {
-            Some(primary_tag) => primary_tag,
-            None => tagged_file
-                .first_tag()
-                .expect("ERROR: could not find any tags!"),
-        };
-        let title = tag
-            .get_string(ItemKey::TrackTitle)
-            .unwrap_or("Untitled")
-            .to_string();
-        let artists: Vec<String> = tag
-            .get_strings(ItemKey::TrackArtist)
-            .map(|s| s.to_owned())
-            .collect();
-        let artist = artists.join(", ");
-        let album = tag
-            .get_string(ItemKey::AlbumTitle)
-            .unwrap_or("Unknown Album")
-            .to_string();
+        let tag = tagged_file
+            .primary_tag()
+            .or_else(|| tagged_file.first_tag());
+
+        let title;
+        let artist;
+        let album;
+
+        if let Some(tag) = tag {
+            title = tag
+                .get_string(ItemKey::TrackTitle)
+                .unwrap_or("Untitled")
+                .to_string();
+
+            let artists: Vec<String> = tag
+                .get_strings(ItemKey::TrackArtist)
+                .map(|s| s.to_owned())
+                .collect();
+
+            artist = if artists.is_empty() {
+                "Unknown Artist".to_string()
+            } else {
+                artists.join(", ")
+            };
+
+            album = tag
+                .get_string(ItemKey::AlbumTitle)
+                .unwrap_or("Unknown Album")
+                .to_string();
+        } else {
+            title = path
+                .file_stem()
+                .and_then(|s| s.to_str())
+                .unwrap_or("Untitled")
+                .to_string();
+
+            artist = "Unknown Artist".to_string();
+            album = "Unknown Album".to_string();
+        }
+
         let duration = tagged_file.properties().duration().as_secs();
         let size = file_metadata.len();
         let modified = file_metadata
@@ -91,6 +144,7 @@ impl Scanner {
             size,
         })
     }
+
 
     fn scan_folder(
         &mut self,
