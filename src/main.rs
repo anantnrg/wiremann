83872817d66233b0ee146ecc1de8f3bbc6903406
Anant Narayan <anantnrg@gmail.com>
2026-04-05 15:23:32 +0530

    feat/imporve_scan_perf: start fixing up scanner

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,13 @@ pub mod errors;
 pub mod library;
 mod queue;
 pub mod scanner;
+mod scanner_v2;
 pub mod ui;
 mod worker_config;
-mod scanner_v2;
 
 use crate::scanner_v2::{ScannerV2, ScannerV2Command};
 use errors::AppError;
 
 fn main() -> Result<(), AppError> {
-    let (scanner, cmd_tx, _event_rx) = ScannerV2::new();
-
-    let handle = scanner.start();
-
-    let path = "E:\\music\\$UMH4RD$H1T".into();
-
-    cmd_tx.send(ScannerV2Command::ScanFolder(path)).unwrap();
-
-    handle.join().unwrap();
-
-    Ok(())
-    // app::run()
+    app::run()
 }
